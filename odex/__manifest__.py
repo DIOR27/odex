d9518c53ec7a646c4896b119e60967eb7be5dde9
Diego Orellana Ávila <diego.orellanaa@ucuenca.edu.ec>
2024-09-17 14:22:46 -0500

    refactor

--- odex/__manifest__.py
+++ odex/__manifest__.py
@@ -13,7 +13,7 @@ Odex is a simple but powerful module for managing regular expressions in fields.
     "category": "Technical",
     "application": False,
     "installable": True,
-    "price": 1.99,
+    "price": 10,
     "currency": "USD",
     "support": "dorellana@agencialava.com",
 }
