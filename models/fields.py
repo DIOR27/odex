0ed34583dbf6c66c428ae313bff054250a418019
Diego Orellana Ávila <38871043+DIOR27@users.noreply.github.com>
2024-09-17 10:51:43 -0500

    fix: null columns

--- models/fields.py
+++ models/fields.py
@@ -2,11 +2,12 @@ from odoo import fields, _
 from odoo.exceptions import ValidationError
 import re
 
-# === Patching for Char ===
 _original_char_init = fields.Char.__init__
 
 
 def custom_char_init(self, string=None, regex=None, **kwargs):
+    if string is None:
+        string = ""
     _original_char_init(self, string=string, **kwargs)
     self.regex = regex
 
@@ -14,12 +15,7 @@ def custom_char_init(self, string=None, regex=None, **kwargs):
 def custom_char_convert_to_cache(self, value, record, validate=True):
     if value and hasattr(self, "regex") and self.regex:
         if not re.match(self.regex, value):
-            raise ValidationError(
-                _(
-                    "The value '%s' does not match the required pattern: %s"
-                    % (value, self.regex)
-                )
-            )
+            raise ValidationError(_(""The value '%s' does not match the required pattern: %s"" % value))
     return super(fields.Char, self).convert_to_cache(value, record, validate=validate)
 
 
