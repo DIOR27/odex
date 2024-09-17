cf38432feb919431d289229d2ce1b8bf4ce10267
Diego Orellana Ávila <diego.orellanaa@ucuenca.edu.ec>
2024-09-17 09:42:30 -0500

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Odex",
+    "summary": "The Odoo RegEx module.",
+    "description": """
+Odex is a simple but powerful module for managing regular expressions in fields.
+    """,
+    "author": "Diego Orellana Ávila",
+    "website": "https://github.com/DIOR27/odex",
+    "category": "Technical",
+    "version": "17.0",
+    "depends": ["base"],
+    "license": "LGPL-3",
+    "application": False,
+    "installable": True,
+}
---
+++ models/fields.py
@@ -0,0 +1,27 @@
+from odoo import fields, _
+from odoo.exceptions import ValidationError
+import re
+
+# === Patching for Char ===
+_original_char_init = fields.Char.__init__
+
+
+def custom_char_init(self, string=None, regex=None, **kwargs):
+    _original_char_init(self, string=string, **kwargs)
+    self.regex = regex
+
+
+def custom_char_convert_to_cache(self, value, record, validate=True):
+    if value and hasattr(self, "regex") and self.regex:
+        if not re.match(self.regex, value):
+            raise ValidationError(
+                _(
+                    "The value '%s' does not match the required pattern: %s"
+                    % (value, self.regex)
+                )
+            )
+    return super(fields.Char, self).convert_to_cache(value, record, validate=validate)
+
+
+fields.Char.__init__ = custom_char_init
+fields.Char.convert_to_cache = custom_char_convert_to_cache
