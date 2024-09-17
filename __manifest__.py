d5199b2554b641bc5f8713f05c66879d3b677a29
Diego Orellana Ávila <diego.orellanaa@ucuenca.edu.ec>
2024-09-17 12:14:49 -0500

    add: index

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 {
     "name": "Odex",
+    "version": "17.0",
+    "license": "LGPL-3",
+    "depends": ["base"],
     "summary": "The Odoo RegEx module.",
     "description": """
 Odex is a simple but powerful module for managing regular expressions in fields.
@@ -8,9 +11,6 @@ Odex is a simple but powerful module for managing regular expressions in fields.
     "author": "Diego Orellana Ávila",
     "website": "https://github.com/DIOR27/odex",
     "category": "Technical",
-    "version": "17.0",
-    "depends": ["base"],
-    "license": "LGPL-3",
     "application": False,
     "installable": True,
 }
